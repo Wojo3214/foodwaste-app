5649463bfc432d36dc8fd1168cc32d02030229a5
Kristine Dzumakajeva <kristinedzu@gmail.com>
2021-11-30 22:26:26 +0100

    updating profile page

--- pages/profile.js
+++ pages/profile.js
@@ -12,8 +12,23 @@ class ProfilePage {
         this.domElement.innerHTML += 
         /*html*/`
         <section id="profile" class="page">
-             <h2>Profile Page</h2>
-             <div >blabla</div>
+            <div class="header">
+                <p class="back-button">back</p>
+                <img class="profile-image" src="../src/img/avatar.svg">
+                <p class="settings-button">sett</p>
+            </div>
+            <h1 class="profile-username padding--top--md text--centered">Ana Smith</h1>
+            <div id="statistics" class="content--horizontal space--around padding--top--md">
+                <div class="statistics content--vertical padding--top--sm padding--bottom--sm">
+                    <p class="statistics-header">Food Shared</p>
+                    <h1 class="food-shared">05</h1>
+                </div>
+                <div class="statistics content--vertical padding--top--sm padding--bottom--sm">
+                <p class="statistics-header">Food Collected</p>
+                    <h1>12/14</h1>
+                </div>
+            </div>
+             <i data-feather="home"></i>
         </section>
         `;
     }  
